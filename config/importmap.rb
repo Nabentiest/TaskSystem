4b2d06c0c5381ce2f928060d0420b574715e5b0e
Nabentiest <soichiro.100m@icloud.com>
2025-08-01 19:19:43 +0900

    modified import map

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,4 @@ pin "@hotwired/turbo-rails", to: "@hotwired--turbo-rails.js" # @8.0.16
 pin "@hotwired/turbo", to: "@hotwired--turbo.js" # @8.0.13
 pin "@rails/actioncable/src", to: "@rails--actioncable--src.js" # @8.0.200
 pin "@rails/ujs", to: "@rails--ujs.js" # @7.1.3
+pin_all_from "app/javascript/controllers", under: "controllers"
